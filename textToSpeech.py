cdf6f4cb6b00cf94fc8e08ffcdbdec080bfde536
Brian McGinns <brian@nobox.io>
2017-06-16 16:17:38 -0500

    Update textToSpeech.py

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,40 +1,43 @@
+#Author: Brian McGinnis
+#Date: June 16 2017
+#Rev: 1.0
+#Notes: implement a way to automatically create the folder and output.wav
+#Must be placed in a folder with a directory called resources
+#recources needs to have the file output.wav
+
 from watson_developer_cloud import TextToSpeechV1
 import json
+import wave
+import pyaudio
+from os.path import join, dirname
 
-class textToSpeech(username, password):
-def __init__(username, password):
-
-  textToSpeech = TextToSpeechV1(
-    username= username,
-    password= password,
-    x_watson_learning_opt_out=True)
+class TextToSpeech:
+	
+	def __init__(self, username, password):
+		self.user = username
+		self.pas = password
+		self.text_to_speech = TextToSpeechV1(username=username, password=password, x_watson_learning_opt_out=True)
   
-  def speak(message):
-	with open(join(dirname(__file__), '../resources/output.wav'), 'wb') as audio_file:
-    		audio_file.write(
-        		text_to_speech.synthesize(message, accept='audio/wav',
-                                  voice="en-US_AllisonVoice"))
-
-	#define stream chunk   
-	chunk = 1024  
-
-	#open a wav format music  
-	f = wave.open(r"../resources/output.wav","rb")  
-	#instantiate PyAudio  
-	p = pyaudio.PyAudio()  
-	#open stream  
-	stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-	                channels = f.getnchannels(),  
-	                rate = f.getframerate(),  
-	                output = True)  
-	#read data  
-	data = f.readframes(chunk)  
-	#play stream  
-	while data:  
-	    stream.write(data)  
-	    data = f.readframes(chunk)  
-	#stop stream  
-	stream.stop_stream()  
-	stream.close()  
-	#close PyAudio  
-	p.terminate()  
+	def speak(self, message):
+		with open(join(dirname(__file__), 'resources/output.wav'), 'wb') as audio_file:
+			audio_file.write(self.text_to_speech.synthesize(message, accept='audio/wav', voice="en-US_AllisonVoice"))
+		#define stream chunk   
+		chunk = 1024
+		#open a wav format music  
+		f = wave.open(r"resources/output.wav","rb")  
+		#instantiate PyAudio  
+		p = pyaudio.PyAudio()  
+		#open stream  
+		stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+		channels = f.getnchannels(), rate = f.getframerate(), output = True)  
+		#read data  
+		data = f.readframes(chunk)  
+		#play stream  
+		while data:  
+			stream.write(data)  
+			data = f.readframes(chunk)  
+			#stop stream  
+		stream.stop_stream()  
+		stream.close()  
+			#close PyAudio  
+		p.terminate()
