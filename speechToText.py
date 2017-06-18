c007d2e0c7639de2ae8c9d2f28a30f83cee2936c
brandon-gong <brandon.gong@brandongong.org>
2017-06-18 09:28:54 -0500

    Fix global name thing

--- speechToText.py
+++ speechToText.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #Author: Brian McGinnis
 #Date: June 17 2017
 #Rev: 1.0
@@ -8,22 +10,22 @@ from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
 
 class SpeechToText:
-	
-	def __init__(self, username, password):
-		self.user = username
-		self.pas = password
-		self.speech_to_text = SpeechToTextV1(username=username, password=password, x_watson_learning_opt_out=True)
-  
 
-	def s2t(self):
-		if not os.path.exists('resources/speech.wav'):
-			return 'I hear nothing'
-		with open(join(dirname(__file__), 'resources/speech.wav'), 'rb') as audio_file:
-			jsn = json.loads(json.dumps(speech_to_text.recognize( audio_file, content_type='audio/wav', timestamps=True, word_confidence=True), indent=2))
-			#print(jsn['results'][0]['alternatives'][0]['transcript'])
-			return jsn['results'][0]['alternatives'][0]['transcript']
-		
-	
+    def __init__(self, username, password):
+        self.user = username
+        self.pas = password
+        self.speech_to_text = SpeechToTextV1(username=username, password=password, x_watson_learning_opt_out=True)
+
+
+    def s2t(self):
+        if not os.path.exists('resources/speech.wav'):
+            return 'I hear nothing'
+        with open(join(dirname(__file__), 'resources/speech.wav'), 'rb') as audio_file:
+            jsn = json.loads(json.dumps(self.speech_to_text.recognize( audio_file, content_type='audio/wav', timestamps=True, word_confidence=True), indent=2))
+            #print(jsn['results'][0]['alternatives'][0]['transcript'])
+            return jsn['results'][0]['alternatives'][0]['transcript']
+
+
 sst = SpeechToText('1f900d96-d3fa-40f0-ab53-9a600f931796', 'GAyuVqI1EEMv')
 print(sst.s2t())
 
