fdbaa92910338d7a387b43ffe293276d16c782cb
brandon-gong <brandon.gong@brandongong.org>
2017-06-21 13:30:46 -0500

    GYEFIGIYFEIUGEFFIUGEFEKUEFBJKEFGUFWKFWVKUVFWKVUAVKFHFHDSF

--- speechrecorder.py
+++ speechrecorder.py
@@ -21,172 +21,180 @@ import time
 import wave
 import pyaudio
 
-# Declare a few constants.
+class SpeechRecorder:
 
-# Mic stuff.
-CHANNELS = 1
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-RATE = 16000
+    # Declare a few constants.
 
-# Previous audio to add on to the beginning
-# in seconds. Added in order to account for possibly cutting off
-# the very beginning of the user's phrase.
-INIT_PADDING = 0.5
+    # Mic stuff.
+    CHANNELS = 1
+    CHUNK = 16384
+    FORMAT = pyaudio.paInt16
+    RATE = 48000
 
-# Sound threshold.  All sounds above this threshold are considered
-# as speech, while below is considered as silence.
-THRESHOLD = 2500
+    # Previous audio to add on to the beginning
+    # in seconds. Added in order to account for possibly cutting off
+    # the very beginning of the user's phrase.
+    INIT_PADDING = 0.5
 
-# The amount of silence allowed after a sound that passes the
-# threshold in seconds.
-SILENCE_LIMIT = 2
+    # Sound threshold.  All sounds above this threshold are considered
+    # as speech, while below is considered as silence.
+    THRESHOLD = 1200
+
+    # The amount of silence allowed after a sound that passes the
+    # threshold in seconds.
+    SILENCE_LIMIT = 2
+
+    def __init__(self, channels=1, chunk=16384, format=pyaudio.paInt16, rate=48000, init_padding=0.5, threshold=1200, silence_limit=2, autothresh=False):
+        self.CHANNELS = channels
+        self.CHUNK = chunk
+        self.FORMAT = format
+        self.RATE = rate
+        self.INIT_PADDING = init_padding
+        self.THRESHOLD = threshold
+        self.SILENCE_LIMIT = silence_limit
+        if autothresh:
+            self.auto_threshold()
+
+    # automatically calculate threshold.
+    # Parameters:
+    #   samples: number of chunks to read from microphone.
+    #   avgintensities: the top x% of the highest intensites read to be averaged.
+    #                   By default, the top 20% of the highest intensities will be
+    #                   averaged together.
+    #   padding: how far above the average intensity the voice should be.
+    # TODO: check to make sure this is actually beneficial to performance.
+    def auto_threshold(self, samples=50, avgintensities=0.2, padding=100):
+        if __debug__:
+            print("Auto-thresholding...")
+
+        # start a stream.
+        #
+        # TODO: if we are to wrap these functions in a class, maybe
+        # we should just create one pyaudio stream and open it in the
+        # constructor.
+        p = pyaudio.PyAudio()
+        stream = p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True, frames_per_buffer=self.CHUNK)
+
+        # Get a number of chunks from the stream as determined by the samples arg,
+        # and calculate intensity.
+        #intensities = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4)))
+        #               for x in range(samples)]
+        intensities = [math.sqrt(abs(audioop.avg(stream.read(self.CHUNK), 4)))]
+
+        # sort the list from greatest to least.
+        intensities = sorted(intensities, reverse=True)
+
+        # get the first avgintensities percent values from the list.
+        self.THRESHOLD = sum( intensities[:int(samples * avgintensities)] ) / int(samples * avgintensities) + padding
+
+        # clean up
+        stream.close()
+        p.terminate()
+
+        if __debug__:
+            print("Threshold: ", self.THRESHOLD)
 
 
-# automatically calculate threshold.
-# Parameters:
-#   samples: number of chunks to read from microphone.
-#   avgintensities: the top x% of the highest intensites read to be averaged.
-#                   By default, the top 20% of the highest intensities will be
-#                   averaged together.
-#   padding: how far above the average intensity the voice should be.
-# TODO: check to make sure this is actually beneficial to performance.
-def auto_threshold(samples=50, avgintensities=0.2, padding=100):
-    if __debug__:
-        print("Auto-thresholding...")
+    # Gets the phrase from the user.
+    # Returns the filename of the .wav file.
+    def get_phrase(self, threshold=THRESHOLD, framerate=RATE):
 
-    # start a stream.
-    #
-    # TODO: if we are to wrap these functions in a class, maybe
-    # we should just create one pyaudio stream and open it in the
-    # constructor.
-    p = pyaudio.PyAudio()
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK
-    )
+        # name of output file.
+        filename = 'file.wav'
 
-    # Get a number of chunks from the stream as determined by the samples arg,
-    # and calculate intensity.
-    intensities = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4)))
-                   for x in range(samples)]
+        # Start a pyaudio stream.
+        p = pyaudio.PyAudio()
+        stream = p.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            #input_device_index = 1,
+            frames_per_buffer=self.CHUNK
+        )
 
-    # sort the list from greatest to least.
-    intensities = sorted(intensities, reverse=True)
+        if __debug__:
+            print("Mic is up and listening.")
 
-    # get the first avgintensities percent values from the list.
-    THRESHOLD = sum( intensities[:int(samples * avgintensities)] ) / int(samples * avgintensities) + padding
+        # list to hold all of the frames of user's speech.
+        speech = []
 
-    # clean up
-    stream.close()
-    p.terminate()
+        # this_chunk holds the current chunk of data.
+        this_chunk = None
 
-    if __debug__:
-        print("Threshold: ", THRESHOLD)
+        # deque containing SILENCE_LIMIT seconds of frames when played
+        # at RATE.
+        silence_buffer = deque(maxlen=int(self.SILENCE_LIMIT * self.RATE / self.CHUNK))
 
+        # deque containing INIT_PADDING seconds of frames when played at
+        # RATE.
+        init_padding = deque(maxlen=int(self.INIT_PADDING * self.RATE / self.CHUNK))
 
-# Gets the phrase from the user.
-# Returns the filename of the .wav file.
-def get_phrase(threshold=THRESHOLD, framerate=RATE):
+        # Have we started recording yet?
+        started = False
 
-    # name of output file.
-    filename = ''
+        while True:
+            # read a new chunk of data from the stream.
+            this_chunk = stream.read(self.CHUNK)
 
-    # Start a pyaudio stream.
-    p = pyaudio.PyAudio()
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK
-    )
+            # calculate the average intensity for this chunk and append.
+            silence_buffer.append(math.sqrt(abs(audioop.avg(this_chunk, 4))))
 
-    if __debug__:
-        print("Mic is up and listening.")
+            # if at least one of the values in silence_buffer is above
+            # the threshold, then keep appending to the speech list.
+            if sum([x > threshold for x in silence_buffer]) > 0:
 
-    # list to hold all of the frames of user's speech.
-    speech = []
+                # if we haven't started already, end the listening phase
+                if not started:
+                    if __debug__:
+                        print("Threshold passed. Recording started.")
+                    started = True
 
-    # this_chunk holds the current chunk of data.
-    this_chunk = None
+                # Append this chunk to the speech list.
+                speech.append(this_chunk)
 
-    # deque containing SILENCE_LIMIT seconds of frames when played
-    # at RATE.
-    silence_buffer = deque(maxlen=SILENCE_LIMIT * RATE / CHUNK)
+            # if already started but nothing above threshold in silence
+            # buffer, end recording and write to file.
+            elif started is True:
 
-    # deque containing INIT_PADDING seconds of frames when played at
-    # RATE.
-    init_padding = deque(maxlen=INIT_PADDING * RATE / CHUNK)
-
-    # Have we started recording yet?
-    started = False
-
-    while True:
-        # read a new chunk of data from the stream.
-        this_chunk = stream.read(CHUNK)
-
-        # calculate the average intensity for this chunk and append.
-        silence_buffer.append(math.sqrt(abs(audioop.avg(this_chunk, 4))))
-
-        # if at least one of the values in silence_buffer is above
-        # the threshold, then keep appending to the speech list.
-        if sum([x > threshold for x in silence_buffer]) > 0:
-
-            # if we haven't started already, end the listening phase
-            if not started:
                 if __debug__:
-                    print("Threshold passed. Recording started.")
-                started = True
+                    print("Maximum silence reached.")
 
-            # Append this chunk to the speech list.
-            speech.append(this_chunk)
+                # generate a filename for temporary speech file
+                filename = 'resources/speech.wav'
 
-        # if already started but nothing above threshold in silence
-        # buffer, end recording and write to file.
-        elif started is True:
+                # concat bytes
+                data = ''.join(list(init_padding) + speech)
 
-            if __debug__:
-                print("Maximum silence reached.")
+                # Write!
+                wf = wave.open(filename, 'wb')
+                wf.setnchannels(1)
+                wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+                wf.setframerate(framerate)
+                wf.writeframes(data)
+                wf.close()
+                break
 
-            # generate a filename for temporary speech file
-            filename = 'temp_' + str(int(time.time())) + '.wav'
+            else:
+                # Haven't started yet, but also nothing above threshold;
+                # keep listening.
+                init_padding.append(this_chunk)
 
-            # concat bytes
-            data = ''.join(list(init_padding) + speech)
+        if __debug__:
+            print("Done.  Closing stream.")
+        stream.close()
+        p.terminate()
 
-            # Write!
-            wf = wave.open(filename, 'wb')
-            wf.setnchannels(1)
-            wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-            wf.setframerate(framerate)
-            wf.writeframes(data)
-            wf.close()
-            break
+        return filename
 
-        else:
-            # Haven't started yet, but also nothing above threshold;
-            # keep listening.
-            init_padding.append(this_chunk)
+    # process the phrase, as hinted by the name of the function
+    # Possibly call Watson STT here.
 
-    if __debug__:
-        print("Done.  Closing stream.")
-    stream.close()
-    p.terminate()
-
-    return filename
-
-# process the phrase, as hinted by the name of the function
-# Possibly call Watson STT here.
-
-
-def process_phrase(f):
-    pass  # process code here
+    def process_phrase(self, f):
+        pass  # process code here
 
 
 if __name__ == '__main__':
-    auto_threshold()
-    get_phrase()
+    #auto_threshold()
+    sr = SpeechRecorder()
+    sr.get_phrase()
