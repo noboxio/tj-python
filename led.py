5c6ef2515aa4b3d45886d4d5a1d6582fa52254e5
Brian McGinnis <brian@nobox.io>
2017-06-21 00:45:19 +0000

    LED.py works now

--- led.py
+++ led.py
@@ -1,7 +1,5 @@
 """
 Author: Brian McGinnis
-Credit to Tony DiCola (tony@tonydicola.com) for the Strobe and wheel methods. They were pulled from strandtest.py
-
 """
 
 import time
@@ -9,49 +7,41 @@ from neopixel import *
 
 class led:
 
-
-    
     def __init__(self):
-            # LED strip configuration:
-        LED_COUNT      = 1      # Number of LED pixels.
-        LED_PIN        = 10      # GPIO pin connected to the pixels (18 uses PWM!).
-        #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-        LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-        LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-        LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-        LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-        LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-        LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
-	self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+        LED_COUNT = 16
+        LED_PIN = 10
+        LED_FREQ_HZ = 800000
+        LED_DMA = 5
+        LED_BRIGHTNESS = 255
+        LED_INVERT = False
+        LED_CHANNEL = 0
+        LED_STRIP = ws.WS2811_STRIP_RGB
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
         self.strip.begin()
-        
+
     def strobe(self):
         wait_ms = 50
-	"""Rainbow movie theater light style chaser animation."""
+
         for j in range(256):
-		for q in range(3):
-			for i in range(0, self.strip.numPixels(), 3):
-				self.strip.setPixelColor(i+q, self.wheel((i+j) % 255))
-			self.strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, self.strip.numPixels(), 3):
-				self.strip.setPixelColor(i+q, 0)
+            for i in range(0, self.strip.numPixels(), 3):
+                self.strip.setPixelCOlor(i+q, self.wheel((i+j) % 255))
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, self.strip.numPixels(), 3):
+                self.strip.setPixelColor(i+q, 0)
 
     def wheel(self,pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
+        if pos < 85:
+            return Color(pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+            pos -= 85
+            return Color(255 - pos * 3, 0, pos * 3)
+        else:
+            pos -= 170
+            return Color(0, pos * 3, 255 - pos * 3)
 
-    def customColor(self):
-        self.strip.setPixelColorRGB(0,255,0,0)
+    def customColor(self, r, g, b):
+        self.strip.setPixelColorRGB(0, r, g, b)
+        self.strip.show()
 
-l = led()
-#l.strobe()
-l.customColor()
-
+    
