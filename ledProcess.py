5ba3384ba96bb86e556298c3285b9fb8f3244652
Patrick McGinnis <patrick@nobox.io>
2017-06-22 20:56:39 -0500

    created ledProcess instead of using threads so that they can be killed

---
+++ ledProcess.py
@@ -0,0 +1,58 @@
+"""
+Author: Brian McGinnis and Patrick McGinnis
+"""
+
+import time
+from neopixel import *
+from multiprocessing import Process
+import led
+
+
+
+
+class LedProcess(threading):
+
+    def __init__(self, led):
+        self.led = led
+        self.process = None
+        
+            
+    def strobe(self):
+        self.__clearProcess__()
+        self.process = threading.Thread(target = self.led.strobe)
+        self.process.start()
+                
+    def wheel(self,pos):
+        self.__clearProcess__()
+        self.process = threading.Thread(target = self.led.wheel, kwargs={'pos':pos})
+        self.process.start()
+        
+    def customColor(self, r, g, b):
+        self.__clearProcess__()
+        self.process = threading.Thread(target = self.led.customColor, kwargs={'r':r,'g':g,'b':b})
+        self.process.start()
+        
+    def __clearThread__(self):
+        if self.process != None:
+            self.process.terminate()
+            
+    def stop(self):
+        self.__clearProcess__()
+
+
+"""
+
+l = LedThread("ID", "NAME")
+l.daemon = True
+l.start()
+
+print("sleeping")
+time.sleep(5)
+print("dome sleepoing")
+l.changename("hey hey")
+print("name changed")
+
+
+print("sleeping again")
+time.sleep(5)
+"""
