4315a8f3adfd1f42238ec5be5e394519f3645c31
Patrick McGinnis <patrick@nobox.io>
2017-07-11 11:29:56 -0500

    making major changes to the way the modules are layed out

---
+++ watsonServices/__init__.py
@@ -0,0 +1,32 @@
+import ConfigParser
+import io
+from conversation import *
+from textToSpeech import *
+from streaming import *
+import time
+
+
+
+# read in the config file
+auth = ConfigParser.RawConfigParser(allow_no_value=True)
+auth.read("auth")
+
+stt = streaming.StreamingSTT(
+    # replace with speech to text credentials username
+    auth.get("stt", "username"),
+    # replace with speech to text credentials password
+    auth.get("stt", "password"))
+
+tts = textToSpeech.TextToSpeech(
+    # replace with text to speech credentials username
+    auth.get("tts", "username"),
+    # replace with text to speech credentials password
+    auth.get("tts", "password"))
+
+convo = conversation.Conversation(
+    # replace with conversation credentials username
+    auth.get("convo", "username"),
+    # replace with conversation credentials password
+    auth.get("convo", "password"),
+    # replace with workspace ID.
+    auth.get("convo","workspaceid"))
