755901b499c156a9c210ff330e81de5041b5bbcb
Patrick McGinnis <patrickm@RNSD.Local>
2017-07-11 13:58:48 -0500

    test

--- music.py
+++ music.py
@@ -12,11 +12,14 @@
 888  888  "Y88P"  88888P"   "Y88P"  888  888     http://github.com/noboxio
 
 Author: Brian McGinnis and Patrick McGinnis
-Date: 6/23/17
+Date: 7/11/17
+atom-timestamp:update-timestamp
 """
 
 import time
 import subprocess
+from multiprocessing import Process
+
 
 
 class Music:
@@ -41,7 +44,6 @@ class Music:
         self.process = subprocess.Popen("exec " + self.cmd,
                                         stdout=subprocess.PIPE, shell=True)
 
-
     def stop(self):
         """Stop this playing song.
 
@@ -49,3 +51,38 @@ class Music:
         and then return nothing
         """
         self.process.kill()
+
+
+
+
+
+class MusicManager():
+    """MusicManager is basically a manager for the music objects.
+
+    it functions as a process so that the song can be started, stopped or
+    whatever whenever
+    """
+
+    def __init__(self, music):
+        """Create a MusicManager type.
+
+        music -- Music object that has been created to be played
+
+        TODO: don't require it to be passed a song, allow it to load a
+        library of songs.
+        """
+        self.music = music
+        self.process = None
+
+
+    def execute_command(self, command):
+        """Execute a command in text form"""
+        self.__clearProcess__()
+        self.process = Process(target=eval("self.music." + command))
+        self.process.start()
+
+
+    def __clearProcess__(self):
+        """Clear any exisiting Music process so that it can be terminated."""
+        if self.process is not None:
+            self.process.terminate()
