981908f09d90f93c809b26f9f3fc7ffbe1bd20c9
Patrick McGinnis <datcomputerguy@gmail.com>
2017-07-14 13:59:09 -0500

    changed to text

--- tjbot.py
+++ tjbot.py
@@ -207,7 +207,7 @@ class TJBott:
 def console_input():
     print("asdfasdfasdfasdfasdffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
     while(1):
-        text = eval(input("COMMAND: "))
+        text = eval('input("COMMAND: ")')
         #tjbot.process_response(text)
         print("text  " + text)
         time.sleep(.25)
