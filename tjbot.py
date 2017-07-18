72bb9278241a7ed7a943e0fea1a2059f617fe31e
Patrick McGinnis <patrick@nobox.io>
2017-07-18 18:16:19 -0500

    removed crazy character

--- tjbot.py
+++ tjbot.py
@@ -132,7 +132,7 @@ class TJBot(threading.Thread):
                 print("sending command to led")
                 cmd = cmd.replace('led.','',1)
                 self.led_manager.add_command(cmd)
-.
+
             if 'arm.' in cmd:
                 cmd = cmd.replace('arm.','',1)
                 self.servo_manager.execute_command(cmd)
