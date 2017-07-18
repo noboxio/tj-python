4cfd637c17326478a08faf8804a64079384b02ac
Patrick McGinnis <patrickm@RNSD.Local>
2017-07-18 15:14:04 -0500

    commands for conversation

---
+++ make_list_for_conversation.py
@@ -0,0 +1,31 @@
+
+
+
+import music
+import servo
+import music
+
+
+
+def main():
+    """Main method creates a TJ bot and starts it along with the console_input.
+
+    main method
+    """
+    line = "================================================================================"
+    print(line)
+    print("Commands for MusicManager")
+    print(line)
+
+    for m in dir(music.MusicManager())
+        help(m)
+
+
+
+    #dir(music)
+    #dir(servo)
+    #dir(music)
+
+
+if __name__ == "__main__":
+    main()
