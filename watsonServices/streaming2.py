367d5be49dbeb3c1169413fdfb455c47a7ba2f1d
brandon-gong <brandon.gong@brandongong.org>
2017-07-28 14:40:47 -0500

    Update streaming2.py

--- watsonServices/streaming2.py
+++ watsonServices/streaming2.py
@@ -207,8 +207,9 @@ class StreamingSTT:
         if "results" in data:
 
             # are those results final?
-            # BUG: if you don't talk, this field won't exist,
-            # and bad things will happen.
+            # BUG: if you don't talk, this field won't exist in the dict,
+            # and bad things will happen, and scary things will show up in
+            # the terminal.
             if data["results"][0]["final"]:
                 self.FINAL.append(data)
 
