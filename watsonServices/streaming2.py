cd37c929e7d5a9e899979bfdd7af53dca17c1485
brandon-gong <brandon.gong@brandongong.org>
2017-07-28 13:17:20 -0500

    Update streaming2.py

--- watsonServices/streaming2.py
+++ watsonServices/streaming2.py
@@ -112,7 +112,7 @@ class StreamingSTT:
     def get_rate(self):
         return self.RATE
 
-    # Set silencec thresholdd
+    # Set silencec threshold
     def set_threshold(self, threshold):
         self.THRESHOLD = threshold
 
