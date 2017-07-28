73c149c37b8d8e13489bdefa68a97f4620eef679
brandon-gong <brandon.gong@brandongong.org>
2017-07-28 13:20:14 -0500

    Update streaming2.py

--- watsonServices/streaming2.py
+++ watsonServices/streaming2.py
@@ -59,8 +59,6 @@ class StreamingSTT:
 
     # Constructor.  Basically all you really need is StreamingSTT(<username>,
     # <password>)
-    # Don't worry about setting all of these the first time.
-    # I recommend you use the getters/setters below for cleaner code.
     def __init__(
             self,
             username,
