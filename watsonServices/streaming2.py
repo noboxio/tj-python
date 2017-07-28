301ff8f0001fa34f2c1e5e7b2182f629e2d98981
brandon-gong <brandon.gong@brandongong.org>
2017-07-28 11:02:52 -0500

    Add streaming2

---
+++ watsonServices/streaming2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+
+                  888
+                  888
+                  888
+88888b.   .d88b.  88888b.   .d88b.  888  888
+888 "88b d88""88b 888 "88b d88""88b `Y8bd8P'
+888  888 888  888 888  888 888  888   X88K
+888  888 Y88..88P 888 d88P Y88..88P .d8""8b.     http://nobox.io
+888  888  "Y88P"  88888P"   "Y88P"  888  888     http://github.com/noboxio
+
+
+streaming2.py: rewrite of streaming.py
+
+Author: Brandon Gong
+Date: 7/28/17
+"""
