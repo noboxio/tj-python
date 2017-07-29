fd672e5f8c045bc337f27565de8e8f4d66a5e98b
brandon-gong <brandon.gong@brandongong.org>
2017-07-29 18:34:32 -0500

    Update streaming2.py

--- watsonServices/streaming2.py
+++ watsonServices/streaming2.py
@@ -330,7 +330,7 @@ class StreamingSTT:
                                          on_close=self.on_close)
         self.WS.on_open = self.on_open
 
-        self.SLEEP_TIMER = threading.Timer(self.SLEEP_AFTER * 60, sleep)
+        self.SLEEP_TIMER = threading.Timer(self.SLEEP_AFTER * 60, sleep, False)
 
         # run the websocket
         self.WS.run_forever(
