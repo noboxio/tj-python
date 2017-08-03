5ef06e262b98591ec9068917c7892dff915b366c
brandon-gong <brandon.gong@brandongong.org>
2017-08-03 13:05:15 -0500

    fix header

--- watsonServices/streaming.py
+++ watsonServices/streaming.py
@@ -9,7 +9,9 @@
 888  888 888  888 888  888 888  888   X88K
 888  888 Y88..88P 888 d88P Y88..88P .d8""8b.     http://nobox.io
 888  888  "Y88P"  88888P"   "Y88P"  888  888     http://github.com/noboxio
-streaming2.py: rewrite of streaming.py
+
+streaming.py: Continuous, streaming Speech-To-Text using websockets.
+
 Author: Brandon Gong
 Date: 7/28/17
 """
