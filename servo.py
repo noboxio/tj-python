42c9aaccb8d96461c1b216b6d6b10ad4d824ddb4
Patrick McGinnis <datcomputerguy@gmail.com>
2017-08-04 15:57:50 -0500

    again trying

--- servo.py
+++ servo.py
@@ -52,6 +52,7 @@ class Servo:
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(26, GPIO.OUT)
         self.pwm = GPIO.PWM(26, 50)
+        self.pwm.start(2.5)
 
     def _log(self, message):
         """Print the log message with the object id.
@@ -70,7 +71,6 @@ class Servo:
             self.up()
             self.down()
             times = times - 1
-        self.pwm.stop()
 
     def angle(self, degrees):
         """Set the angle of the servo to a specific angle.
@@ -85,10 +85,10 @@ class Servo:
         if degrees > 180 or degrees < 0:
             print("INVALID ANGLE SPECIFIED.  MUST BE BETWEEN 0 AND 180")
         else:
-            self.pwm.start(map(degrees, 0, 180, 2.5, 12.5))
+            self.pwm.ChangeDutyCycle(map(degrees, 0, 180, 2.5, 12.5))
             print("MAP " + str(degrees) + " : " + str(map(degrees, 0, 180, 2.5, 12.5)))
-            time.sleep(.5)
-            self.pwm.stop()
+            time.sleep(1)
+            #self.pwm.stop()
 
     def up(self):
         """Point the arm up, use this to define the up angle
