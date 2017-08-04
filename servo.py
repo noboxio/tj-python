c489cfd038bebc6e2672d2e6ff657275f0cdbece
Patrick McGinnis <datcomputerguy@gmail.com>
2017-08-04 16:02:30 -0500

    asdf

--- servo.py
+++ servo.py
@@ -87,7 +87,7 @@ class Servo:
         if degrees > 180 or degrees < 0:
             print("INVALID ANGLE SPECIFIED.  MUST BE BETWEEN 0 AND 180")
         else:
-            self.pwm.ChangeDutyCycle(map(degrees, 0, 180, 2.5, 12.5))
+            self.pwm.start(map(degrees, 0, 180, 2.5, 12.5))
             print("MAP " + str(degrees) + " : " + str(map(degrees, 0, 180, 2.5, 12.5)))
             time.sleep(1)
             self.pwm.stop()
