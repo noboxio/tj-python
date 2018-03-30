ef518b706565c3e9e1e3978a7e9b42edf455e6da
Brian McGinns <brian@nobox.io>
2018-03-30 15:38:56 -0500

    Error handler update and TSSL

--- watsonServices/streaming.py
+++ watsonServices/streaming.py
@@ -9,9 +9,7 @@
 888  888 888  888 888  888 888  888   X88K
 888  888 Y88..88P 888 d88P Y88..88P .d8""8b.     http://nobox.io
 888  888  "Y88P"  88888P"   "Y88P"  888  888     http://github.com/noboxio
-
 streaming.py: Continuous, streaming Speech-To-Text using websockets.
-
 Author: Brandon Gong
 Date: 7/28/17
 """
@@ -33,19 +31,22 @@ from websocket._abnf import ABNF
 class StreamingSTT:
 
     # Mic config.
-    CHUNK = 16384
+    CHUNK = 1024
     FORMAT = pyaudio.paInt16
     # It is necessary to keep CHANNELS at 1. Streaming STT does not handle the
     # extra data well and returns unwanted hums.
     CHANNELS = 1
-    RATE = 48000
+    RATE = 44100
 
     # large array of json data returned by watson.
     FINAL = []
 
+    #has stt determined that it is the final response yet?
+    FINALRESPONSE = False
+
     # Threshold: above this point it is considered user speech, below this
     # point it is considered silence.
-    THRESHOLD = 1500
+    THRESHOLD = 1000
 
     # The amount of silence allowed after a sound that passes the
     # threshold in seconds.
@@ -65,8 +66,10 @@ class StreamingSTT:
             password,
             logfile=False,
             loglevel=logging.DEBUG,
-            auto_threshold=True
+            auto_threshold=False
     ):
+        self.current_phrase = ""
+
         self.userpass = ":".join((username, password))
         if logfile:
             logging.basicConfig(filename='streaming.log', level=loglevel)
@@ -134,7 +137,7 @@ class StreamingSTT:
     #   averaged together.
     #   padding: how far above the average intensity the voice should be.
     # TODO: check to make sure this is actually beneficial to performance.
-    def auto_threshold(self, samples=10, avgintensities=0.2, padding=10):
+    def auto_threshold(self, samples=100, avgintensities=0.2, padding=10):
         logging.debug("Auto-thresholding...")
         stream = self.p.open(
             format=self.FORMAT,
@@ -151,6 +154,7 @@ class StreamingSTT:
 
         # sort the list from greatest to least.
         intensities = sorted(intensities, reverse=True)
+        print(intensities)
 
         # get the first avgintensities percent values from the list.
         self.THRESHOLD = sum(intensities[:int(samples * avgintensities)]) / int(
@@ -181,22 +185,30 @@ class StreamingSTT:
         limit_chunks = self.SILENCE_LIMIT * self.RATE / self.CHUNK
 
         while True:
-            #print(str(silence_chunks) + " | " + str(limit_chunks))
-            if silence_chunks >= limit_chunks:
+            if self.FINALRESPONSE:
                 break
 
-            data = stream.read(self.CHUNK, exception_on_overflow=False)
+            #print(str(silence_chunks) + " | " + str(limit_chunks))
+            #if silence_chunks >= limit_chunks:
+            #    break
+            try:
+                data = stream.read(self.CHUNK)#, exception_on_overflow=False)
+            except:
+                stream.stop_stream()
+                stream.close()
+                break
             try:
                 ws.send(data, ABNF.OPCODE_BINARY)
             except:
-
-
                 break
             #print(math.sqrt(abs(audioop.avg(data, 2))) )
-            if math.sqrt(abs(audioop.avg(data, 4))) > self.THRESHOLD:
-                silence_chunks = 0
-            else:
-                silence_chunks += 1
+            #logging.debug(str(math.sqrt(abs(audioop.avg(data, 4)))))
+            #if math.sqrt(abs(audioop.avg(data, 4))) > self.THRESHOLD:
+            #    silence_chunks = 0
+                #logging.debug("silence chunk = 0")
+            #else:
+            #    silence_chunks += 1
+                #logging.debug("silence chunk = " + str(silence_chunks))
 
         # Disconnect the audio stream
         stream.stop_stream()
@@ -243,6 +255,7 @@ class StreamingSTT:
         # load json data into a multidimensional list
         data = json.loads(msg)
 
+
         # are there results?
         if "results" in data:
 
@@ -252,17 +265,131 @@ class StreamingSTT:
             # the terminal.
             if len(data["results"]) != 0:
                 if data["results"][0]["final"]:
+                    self.FINALRESPONSE = True
                     self.FINAL.append(data)
+            try:
+                logging.debug(data['results'][0]['alternatives'][0]['transcript'])
+                self.current_phrase = data['results'][0]['alternatives'][0]['transcript']
+            except:
+                print("no sound heard I guess")
 
-            logging.debug(data['results'][0]['alternatives'][0]['transcript'])
+            if self.FINALRESPONSE:
+                self.current_phrase = ""
+
 
     # print those errors
-    def on_error(self, error):
+    def on_error(self, ws, error):
         logging.error(error)
 
     # inform coder dude that websocket was closed
     def on_close(self, ws):
         logging.debug("Websocket closed.")
+
+    # get_phrase should not be confused with read_audio.
+    # get_phrase should always be called instead of read_audio.
+    # it makes the necessary re-initializations and then calls read_audio
+    # itself.
+    def get_phrase(self):
+
+        # empty out the finals list of the data from last use.
+
+        self.FINAL = []
+        self.FINALRESPONSE = False
+
+        # connect up the websocket
+        headers = {}
+        headers["Authorization"] = "Basic " + base64.b64encode(
+            self.userpass.encode()).decode()
+        url = ("wss://stream.watsonplatform.net//speech-to-text/api/v1/recognize"
+               "?model=en-US_BroadbandModel")
+        self.WS = websocket.WebSocketApp(url,
+                                         header=headers,
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close)
+        self.WS.on_open = self.on_open
+
+        # run the websocket
+        self.WS.run_forever(
+            sslopt={
+                "cert_reqs": ssl.CERT_NONE,
+                "check_hostname": False,
+                "ssl_version": ssl.PROTOCOL_TLSv1_2
+            }
+        )
+
+        # return the parsed result
+        return "".join([i['results'][0]['alternatives'][0]
+                        ['transcript'] for i in self.FINAL])
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        print("Usage: " + sys.argv[0] + " <username> <password> [<timeout>]")
+        sys.exit()
+
+    elif len(sys.argv) > 3:
+        StreamingSTT(sys.argv[1], sys.argv[2], sys.argv[3]).get_phrase()
+
+    else:
+        s = StreamingSTT(sys.argv[1], sys.argv[2], auto_threshold=True)
+        x = s.get_phrase()
+        print(x)
+        print("\n\n\n\nget_phrase can be called as much as you want.\n\n\n\n")
+        s.get_phrase()        # close the websocket
+        ws.close()
+        p.terminate()
+
+    # this callback is used when the connection is activated.
+    # basically initializing and configuring settings and stuff
+    def on_open(self, ws):
+
+        # dump this config dictionary out to JSON
+        data = {
+            "action": "start",
+            "content-type": "audio/l16;rate=%d" % self.RATE,
+            "continuous": True,
+            "interim_results": True,
+            "word_confidence": True,
+            "timestamps": True,
+            "max_alternatives": 3
+        }
+
+        # Send the dictionary through the socket.
+        ws.send(json.dumps(data).encode('utf8'))
+
+        # start a thread to read audio.
+        threading.Thread(target=self.read_audio,
+                         args=(ws, self.TIMEOUT)).start()
+
+    # callback for when we receive a JSON message from watson.
+    # egg: a useless parameter put there to avoid python yelling at me
+    def on_message(self, egg, msg):
+
+        # load json data into a multidimensional list
+        data = json.loads(msg)
+
+        # are there results?
+        if "results" in data:
+
+            # are those results final?
+            if data["results"][0]["final"]:
+                self.FINAL.append(data)
+
+            if __debug__:
+                # printing the many alternatives of what the user said
+                print(data['results'][0]['alternatives'][0]['transcript'])
+
+    # print those errors
+    def on_error(self, error):
+        if __debug__:
+            print(error)
+
+    # inform coder dude that websocket was closed
+    def on_close(self, ws):
+        if __debug__:
+            print("Websocket closed.")
 
     # get_phrase should not be confused with read_audio.
     # get_phrase should always be called instead of read_audio.
@@ -292,7 +419,7 @@ class StreamingSTT:
             sslopt={
                 "cert_reqs": ssl.CERT_NONE,
                 "check_hostname": False,
-                "ssl_version": ssl.PROTOCOL_TLSv1
+                "ssl_version": ssl.PROTOCOL_TLSv1_2
             }
         )
 
@@ -311,7 +438,7 @@ if __name__ == "__main__":
         StreamingSTT(sys.argv[1], sys.argv[2], sys.argv[3]).get_phrase()
 
     else:
-        s = StreamingSTT(sys.argv[1], sys.argv[2], auto_threshold=True)
+        s = StreamingSTT(sys.argv[1], sys.argv[2])
         x = s.get_phrase()
         print(x)
         print("\n\n\n\nget_phrase can be called as much as you want.\n\n\n\n")
